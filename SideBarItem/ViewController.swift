a988f11358205abf1fae870e318292e501f6fb6c
Nick <117998@swcsd.us>
2021-04-16 13:15:41 -0400

    Initial Commit

---
+++ SideBarItem/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideBarItem
+//
+//  Created by Nicholas Centers on 4/16/21.
+//  Copyright © 2021 Nicholas Centers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SideBarItemTests/SideBarItemTests.swift
@@ -0,0 +1,34 @@
+//
+//  SideBarItemTests.swift
+//  SideBarItemTests
+//
+//  Created by Nicholas Centers on 4/16/21.
+//  Copyright © 2021 Nicholas Centers. All rights reserved.
+//
+
+import XCTest
+@testable import SideBarItem
+
+class SideBarItemTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
